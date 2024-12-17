80639ee604bbb6c787cbc8c99d20e6ba4b537641
Emppu <83163481+emppu-dev@users.noreply.github.com>
2024-12-18 01:30:06 +0200

    add: multi country support

--- main.go
+++ main.go
@@ -16,190 +16,6 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-type Items struct {
-	Items                []Item                    `json:"items"`
-	DominantBrand        interface{}               `json:"dominant_brand"`
-	SearchTrackingParams ItemsSearchTrackingParams `json:"search_tracking_params"`
-	Pagination           Pagination                `json:"pagination"`
-	Code                 int64                     `json:"code"`
-}
-
-type Item struct {
-	ID                   int64                    `json:"id"`
-	Title                string                   `json:"title"`
-	Price                Price                    `json:"price"`
-	IsVisible            bool                     `json:"is_visible"`
-	Discount             interface{}              `json:"discount"`
-	BrandTitle           string                   `json:"brand_title"`
-	Path                 string                   `json:"path"`
-	User                 User                     `json:"user"`
-	Conversion           *Conversion              `json:"conversion"`
-	URL                  string                   `json:"url"`
-	Promoted             bool                     `json:"promoted"`
-	Photo                ItemPhoto                `json:"photo"`
-	FavouriteCount       int64                    `json:"favourite_count"`
-	IsFavourite          bool                     `json:"is_favourite"`
-	Badge                interface{}              `json:"badge"`
-	ServiceFee           Price                    `json:"service_fee"`
-	TotalItemPrice       Price                    `json:"total_item_price"`
-	ViewCount            int64                    `json:"view_count"`
-	SizeTitle            string                   `json:"size_title"`
-	ContentSource        ContentSource            `json:"content_source"`
-	Status               Status                   `json:"status"`
-	IconBadges           []interface{}            `json:"icon_badges"`
-	SearchTrackingParams ItemSearchTrackingParams `json:"search_tracking_params"`
-}
-
-type Conversion struct {
-	SellerPrice    string         `json:"seller_price"`
-	SellerCurrency SellerCurrency `json:"seller_currency"`
-	BuyerCurrency  CurrencyCode   `json:"buyer_currency"`
-	FxRoundedRate  string         `json:"fx_rounded_rate"`
-	FxBaseAmount   string         `json:"fx_base_amount"`
-	FxMarkupRate   string         `json:"fx_markup_rate"`
-}
-
-type ItemPhoto struct {
-	ID                  int64          `json:"id"`
-	ImageNo             int64          `json:"image_no"`
-	Width               int64          `json:"width"`
-	Height              int64          `json:"height"`
-	DominantColor       string         `json:"dominant_color"`
-	DominantColorOpaque string         `json:"dominant_color_opaque"`
-	URL                 string         `json:"url"`
-	IsMain              bool           `json:"is_main"`
-	Thumbnails          []Thumbnail    `json:"thumbnails"`
-	HighResolution      HighResolution `json:"high_resolution"`
-	IsSuspicious        bool           `json:"is_suspicious"`
-	FullSizeURL         string         `json:"full_size_url"`
-	IsHidden            bool           `json:"is_hidden"`
-	Extra               Extra          `json:"extra"`
-}
-
-type Extra struct {
-}
-
-type HighResolution struct {
-	ID          string `json:"id"`
-	Timestamp   int64  `json:"timestamp"`
-	Orientation *int64 `json:"orientation"`
-}
-
-type Thumbnail struct {
-	Type         Type   `json:"type"`
-	URL          string `json:"url"`
-	Width        int64  `json:"width"`
-	Height       int64  `json:"height"`
-	OriginalSize *bool  `json:"original_size"`
-}
-
-type Price struct {
-	Amount       string       `json:"amount"`
-	CurrencyCode CurrencyCode `json:"currency_code"`
-}
-
-type ItemSearchTrackingParams struct {
-	Score          int64       `json:"score"`
-	MatchedQueries interface{} `json:"matched_queries"`
-}
-
-type User struct {
-	ID         int64      `json:"id"`
-	Login      string     `json:"login"`
-	ProfileURL string     `json:"profile_url"`
-	Photo      *UserPhoto `json:"photo"`
-	Business   bool       `json:"business"`
-}
-
-type UserPhoto struct {
-	ID                  int64               `json:"id"`
-	Width               int64               `json:"width"`
-	Height              int64               `json:"height"`
-	TempUUID            interface{}         `json:"temp_uuid"`
-	URL                 string              `json:"url"`
-	DominantColor       DominantColor       `json:"dominant_color"`
-	DominantColorOpaque DominantColorOpaque `json:"dominant_color_opaque"`
-	Thumbnails          []Thumbnail         `json:"thumbnails"`
-	IsSuspicious        bool                `json:"is_suspicious"`
-	Orientation         interface{}         `json:"orientation"`
-	HighResolution      HighResolution      `json:"high_resolution"`
-	FullSizeURL         string              `json:"full_size_url"`
-	IsHidden            bool                `json:"is_hidden"`
-	Extra               Extra               `json:"extra"`
-}
-
-type Pagination struct {
-	CurrentPage  int64 `json:"current_page"`
-	TotalPages   int64 `json:"total_pages"`
-	TotalEntries int64 `json:"total_entries"`
-	PerPage      int64 `json:"per_page"`
-	Time         int64 `json:"time"`
-}
-
-type ItemsSearchTrackingParams struct {
-	SearchCorrelationID   string `json:"search_correlation_id"`
-	SearchSessionID       string `json:"search_session_id"`
-	GlobalSearchSessionID string `json:"global_search_session_id"`
-}
-
-type ContentSource string
-
-const (
-	Search ContentSource = "search"
-)
-
-type CurrencyCode string
-
-const (
-	Eur CurrencyCode = "EUR"
-)
-
-type SellerCurrency string
-
-const (
-	Dkk SellerCurrency = "DKK"
-	Pln SellerCurrency = "PLN"
-	Sek SellerCurrency = "SEK"
-)
-
-type Type string
-
-const (
-	Thumb100     Type = "thumb100"
-	Thumb150     Type = "thumb150"
-	Thumb150X210 Type = "thumb150x210"
-	Thumb20      Type = "thumb20"
-	Thumb310     Type = "thumb310"
-	Thumb310X430 Type = "thumb310x430"
-	Thumb364X428 Type = "thumb364x428"
-	Thumb428X624 Type = "thumb428x624"
-	Thumb50      Type = "thumb50"
-	Thumb624X428 Type = "thumb624x428"
-	Thumb70X100  Type = "thumb70x100"
-)
-
-type Status string
-
-const (
-	Good           Status = "Good"
-	NewWithTags    Status = "New with tags"
-	NewWithoutTags Status = "New without tags"
-	Satisfactory   Status = "Satisfactory"
-	VeryGood       Status = "Very good"
-)
-
-type DominantColor string
-
-const (
-	The31Abc2 DominantColor = "#31abc2"
-)
-
-type DominantColorOpaque string
-
-const (
-	C1E6Ed DominantColorOpaque = "#C1E6ED"
-)
-
 type Product struct {
 	Title string `json:"title"`
 	Price struct {
@@ -221,9 +37,14 @@ type Response struct {
 	Items []Product `json:"items"`
 }
 
+type ItemPhoto struct {
+	DominantColor string `json:"dominant_color"`
+	URL           string `json:"url"`
+}
+
 var foundTotal int
 
-func getAccessToken() string {
+func getAccessToken(country string) string {
 	jar, err := cookiejar.New(nil)
 	if err != nil {
 		log.Fatalf("Error creating cookie jar: %v", err)
@@ -232,8 +53,7 @@ func getAccessToken() string {
 	client := &http.Client{
 		Jar: jar,
 	}
-
-	urlStr := "https://www.vinted.fi"
+	urlStr := "https://www.vinted." + country
 	resp, err := client.Get(urlStr)
 	if err != nil {
 		log.Fatalf("Error getting URL: %v", err)
@@ -260,10 +80,15 @@ func main() {
 		log.Fatalf("Error loading .env file")
 	}
 
-	hakusana := os.Getenv("HAKUSANA")
+	hakusana := os.Getenv("SEARCH")
+	country := os.Getenv("COUNTRY")
 	discordWebhook := os.Getenv("DISCORD_WEBHOOK")
 	telegramBotToken := os.Getenv("TELEGRAM_BOT_TOKEN")
 	telegramChatID := os.Getenv("TELEGRAM_CHAT_ID")
+
+	if country == "" {
+		log.Fatalf("COUNTRY not defined in .env file")
+	}
 
 	if discordWebhook == "" && (telegramBotToken == "" || telegramChatID == "") {
 		fmt.Println("Notification method not defined. Please set either Discord Webhook or Telegram Bot Token and Chat ID, or both.")
@@ -281,13 +106,12 @@ func main() {
 
 	client := &fasthttp.Client{}
 
-	accessToken := getAccessToken()
-	//fmt.Println("Access Token:", accessToken)
+	accessToken := getAccessToken(country)
 
 	for {
 		foundTotal = 0
 		req := fasthttp.AcquireRequest()
-		req.SetRequestURI("https://www.vinted.fi/api/v2/catalog/items?page=1&per_page=96&search_text=" + hakusana + "&order=newest_first")
+		req.SetRequestURI("https://www.vinted." + country + "/api/v2/catalog/items?page=1&per_page=96&search_text=" + hakusana + "&order=newest_first")
 		req.Header.SetMethod("GET")
 		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
 		req.Header.Set("Accept", "application/json")
@@ -299,7 +123,6 @@ func main() {
 			log.Fatalf("Error: %s\n", err)
 			continue
 		}
-		//fmt.Println("Response: ", string(resp.Body()))
 
 		var response Response
 		body := resp.Body()
@@ -350,16 +173,19 @@ func sendDiscordNotification(webhook string, product Product) {
 		}
 	}
 	ticks := "```"
-	payload := fmt.Sprintf(`{"content": null,"embeds": [{"title": "%s","url": "%s","color": %d,"fields": [{"name": "Hinta","value": "%s%s EUR%s","inline": true},{"name": "Merkki","value": "%s%s%s","inline": true},{"name": "Koko","value": "%s%s%s","inline": true},{"name": "Kunto","value": "%s%s%s","inline": true}],"image": {"url": "%s"}}]}`,
-		product.Title, product.URL, color, ticks, product.TotalItemPrice.Amount, ticks, ticks, product.BrandTitle, ticks, ticks, product.SizeTitle, ticks, ticks, product.Status, ticks, product.ImageURL.URL)
+	payload := fmt.Sprintf(`{"content": null,"embeds": [{"title": "%s","url": "%s","color": %d,"fields": [{"name": "Price","value": "%s%s %s%s","inline": true},{"name": "Brand","value": "%s%s%s","inline": true},{"name": "Size","value": "%s%s%s","inline": true},{"name": "Condition","value": "%s%s%s","inline": true}],"image": {"url": "%s"}}]}`,
+		product.Title, product.URL, color, ticks, product.TotalItemPrice.Amount, product.TotalItemPrice.CurrencyCode, ticks, ticks, product.BrandTitle, ticks, ticks, product.SizeTitle, ticks, ticks, product.Status, ticks, product.ImageURL.URL)
 	sendWebhook(webhook, payload)
 }
 
 func sendTelegramNotification(botToken, chatID string, product Product) {
-	message := fmt.Sprintf("%s\nPrice: %s %s\n%s\n[Photo](%s)",
+	message := fmt.Sprintf("%s\nPrice: %s %s\nBrand: %s\nSize: %s\nCondition: %s\n%s\n[Photo](%s)",
 		product.Title,
-		product.Price.Amount,
-		product.Price.CurrencyCode,
+		product.TotalItemPrice.Amount,
+		product.TotalItemPrice.CurrencyCode,
+		product.BrandTitle,
+		product.SizeTitle,
+		product.Status,
 		product.URL,
 		product.ImageURL.URL)
 
